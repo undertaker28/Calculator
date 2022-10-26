3b131f8cc6c1f4b75d202031bd65a67ea488a8d8
undertaker28 <pavel.starovoytovvv@gmail.com>
2022-10-27 02:48:26 +0300

    Add full logic

---
+++ Calculator/Logic/LogicOperations.swift
@@ -0,0 +1,115 @@
+//
+//  LogicOperations.swift
+//  Calculator
+//
+//  Created by Pavel on 27.10.22.
+//
+
+import Foundation
+
+class LogicOperations {
+
+    private lazy var accumulator = 0.0
+    private lazy var previousAccumulator = 0.0
+    private lazy var previousPriority = 20
+    private lazy var pending: [PendingBinaryOperationInfo] = []
+    private var previousOperation: PendingBinaryOperationInfo?
+    var result: Double {
+        return accumulator
+    }
+
+    private enum Operation {
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double, Int)
+        case percentOperation((Double) -> Double, (Double, Double) -> Double)
+        case equals
+        case clear
+    }
+    
+    // MARK: - Saving information on the deferred operation into the structure: first operator and function
+    private struct PendingBinaryOperationInfo {
+        var function: ((Double, Double) -> Double)
+        var firstOperand: Double
+    }
+
+    private var operations: [String: Operation] = [
+        "⁺∕₋": Operation.unaryOperation{ -$0 },
+        "×": Operation.binaryOperation({ $0 * $1 }, 1),
+        "+": Operation.binaryOperation({ $0 + $1 }, 0),
+        "-": Operation.binaryOperation({ $0 - $1 }, 0),
+        "÷": Operation.binaryOperation({ $0 / $1 }, 1),
+        "=": Operation.equals,
+        "%": Operation.percentOperation({ $0 / 100 }, { $0 * $1 / 100 }),
+        "C": Operation.clear
+    ]
+
+    // MARK: - Setting initial value
+    func setOperand(operand: Double) {
+        accumulator = operand
+    }
+    
+    // MARK: - Perform incoming operation or delay
+    func performOperation(symbol: String, getBothOperandsForBinaryOperation: Bool = true) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .unaryOperation(let function):
+                accumulator = function(accumulator)
+                previousAccumulator = accumulator
+            case .binaryOperation(let function, let priority):
+                if getBothOperandsForBinaryOperation {
+                    if pending.count > 0 {
+                        if previousPriority > priority {
+                            executePendingOperations()
+                        } else if previousPriority == priority {
+                            executeLastPendingOperation()
+                        }
+                    }
+                    pending.append(PendingBinaryOperationInfo(function: function, firstOperand: accumulator))
+                    previousPriority = priority
+                    previousAccumulator = accumulator
+                }
+            case .percentOperation(let oneDigitPercent, let twoDigitPercent):
+                if pending.count > 0 {
+                    print("test")
+                    accumulator = twoDigitPercent(accumulator, previousAccumulator)
+                } else {
+                    accumulator = oneDigitPercent(accumulator)
+                }
+                previousAccumulator = accumulator
+            case .equals:
+                if pending.count > 0 {
+                    if let function = pending.last?.function {
+                        previousOperation = PendingBinaryOperationInfo(function: function, firstOperand: accumulator)
+                    }
+                    executePendingOperations()
+                }
+                else if let operation = previousOperation {
+                    accumulator = operation.function(accumulator, operation.firstOperand)
+                }
+                previousAccumulator = accumulator
+            case .clear:
+                clear()
+            }
+        }
+    }
+    
+    func clear() {
+        accumulator = 0.0
+        previousAccumulator = 0.0
+        pending = []
+        previousOperation = nil
+    }
+    
+    // MARK: - Performing all deferred operations from the array
+    private func executePendingOperations() {
+        while pending.count > 0 {
+            executeLastPendingOperation()
+        }
+    }
+    
+    // MARK: - Performing the last deferred operation
+    private func executeLastPendingOperation() {
+        let operation = pending.removeLast()
+        accumulator = operation.function(operation.firstOperand, accumulator)
+    }
+}
--- Calculator/ViewController/ViewController.swift
+++ Calculator/ViewController/ViewController.swift
@@ -10,14 +10,39 @@ import SnapKit
 
 class ViewController: UIViewController {
     
-    private lazy var digitsAndSymbols = [
-        "C", "⁺∕₋", "%", "÷", "7", "8", "9", "×", "4", "5", "6", "-", "1", "2", "3", "+", "0", ",", "=",
-    ]
+    private lazy var digitsAndSymbols = ["C", "⁺∕₋", "%", "÷", "7", "8", "9", "×", "4", "5", "6", "-", "1", "2", "3", "+", "0", ",", "="]
     private lazy var colorsArray: [UIColor] = [#colorLiteral(red: 0.3040827513, green: 0.3140518367, blue: 0.3736236691, alpha: 1), #colorLiteral(red: 0.295702666, green: 0.3668284416, blue: 0.988561213, alpha: 1), #colorLiteral(red: 0.1794961393, green: 0.1844775975, blue: 0.2185646594, alpha: 1)]
     private lazy var stackOfStacks = UIStackView()
     private lazy var stackArray = [UIStackView]()
     private lazy var buttons = [RoundButton]()
     private lazy var resultLabel = Resultlabel()
+    private lazy var typing = false
+    private lazy var logic = LogicOperations()
+    private lazy var formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 9
+        formatter.groupingSeparator = " "
+        formatter.locale = Locale.current
+        return formatter
+    }()
+    
+    var displayValue: Double? {
+        get {
+            if let text = resultLabel.text, let value = Double(text.replacingOccurrences(of: ",", with: ".")) {
+                return value
+            } else {
+                return nil
+            }
+        }
+        set {
+            if let value = newValue {
+                resultLabel.text = formatter.string(from: NSNumber(value: value))
+            } else {
+                resultLabel.text = "0"
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,21 +89,28 @@ class ViewController: UIViewController {
             case "C", "%", "⁺∕₋":
                 button.backgroundColor = colorsArray[0]
                 button.setTitle(digitsAndSymbols[element], for: .normal)
+                if digitsAndSymbols[element] == "C" {
+                    button.addTarget(self, action: #selector(self.pressC(_:)), for: .touchUpInside)
+                } else {
+                    button.addTarget(self, action: #selector(self.pressOperator(_:)), for: .touchUpInside)
+                }
             case "÷", "×", "-", "+", "=":
                 button.backgroundColor = colorsArray[1]
                 button.setTitle(digitsAndSymbols[element], for: .normal)
+                button.addTarget(self, action: #selector(self.pressOperator(_:)), for: .touchUpInside)
             default:
                 button.backgroundColor = colorsArray[2]
                 button.setTitle(digitsAndSymbols[element], for: .normal)
+                button.addTarget(self, action: #selector(self.pressNumber(_:)), for: .touchUpInside)
             }
             if button.titleLabel?.text == "0" {
                 button.contentEdgeInsets.right = 90
-                button.snp.makeConstraints {
-                    $0.width.equalTo(button.snp.height).multipliedBy(2)
+                button.snp.makeConstraints { buttonZero in
+                    buttonZero.width.equalTo(button.snp.height).multipliedBy(2)
                 }
             } else {
-                button.snp.makeConstraints {
-                    $0.width.equalTo(button.snp.height).multipliedBy(1)
+                button.snp.makeConstraints { normalButton in
+                    normalButton.width.equalTo(button.snp.height).multipliedBy(1)
                 }
             }
             buttons.append(button)
@@ -108,5 +140,39 @@ class ViewController: UIViewController {
         }
     }
     
+    @objc func pressNumber(_ sender: UIButton) {
+        guard let buttonTitle = sender.titleLabel?.text, let displayedText = resultLabel.text else { return }
+        if typing {
+            if buttonTitle != "," || (buttonTitle == "," && displayedText.contains(",") == false) {
+                if displayedText != "0" {
+                    resultLabel.text = displayedText + buttonTitle
+                } else {
+                    resultLabel.text = (buttonTitle == "," ? "0," : buttonTitle)
+                }
+            }
+        } else {
+            resultLabel.text = (buttonTitle == "," ? "0," : buttonTitle)
+        }
+        typing = true
+    }
+    
+    @objc func pressOperator(_ sender: UIButton) {
+        guard let buttonTitle = sender.titleLabel?.text else { return }
+        let needCalculation = typing
+        if typing, let value = displayValue {
+            logic.setOperand(operand: value)
+            typing = false
+        }
+        logic.performOperation(symbol: buttonTitle, getBothOperandsForBinaryOperation: needCalculation)
+        displayValue = logic.result
+    }
+    
+    @objc func pressC(_ sender: UIButton) {
+        if sender.currentTitle == "C" {
+            logic.clear()
+            displayValue = 0
+            typing = false
+        }
+    }
+    
 }
-
